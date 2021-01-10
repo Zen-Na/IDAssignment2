cb5e35555bc5122f933fabdb10d084006856ea91
conanxd45 <conanxd45@gmail.com>
2021-01-10 15:00:16 +0000

    Added PokeDex UI( edited pokedex ui (edits done by me) gender option name description updated pokedex background )
    Added Search Function(
    changed gender / shiny image to only show one image depending on which button selected
    )

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-let url = "https://pokeapi.co/api/v2/pokemon/ditto"
-document.getElementById("imgfemaleshiny").style.display = "none"
-document.getElementById("imgfemale").style.display = "none"
-document.getElementById("imgshiny").style.display = "none"
-document.getElementById("img").style.display = "none"
+document.getElementById("imgfemaleshiny").style.display = "none";
+document.getElementById("imgfemale").style.display = "none";
+document.getElementById("imgshiny").style.display = "none";
+document.getElementById("img").style.display = "none";
 
-btn.addEventListener("click", function() {
-fetch(url)
-  .then(response => response.json()) 
-  .then(data)
-});
-
-function data (e) {
-
-  console.log(e)
-}
 
 submit.addEventListener("click", function() {
+clearImage()
 let search = document.getElementById("srch");
 
 // Image of Pokemon
@@ -25,32 +15,31 @@ fetch(url2)
 .then(function(res){
   
   // show image
-  document.getElementById("img").style.display = "inline"
+  document.getElementById("img").style.display = "inline";
   document.getElementById("img").src = res.sprites.front_default;
   document.getElementById("imgshiny").src = res.sprites.front_shiny;
 
   // Check if female sprites available
   if(res.sprites.front_female == null){
-    document.getElementById("imgfemale").src = "https://www.flaticon.com/svg/static/icons/svg/390/390914.svg"
+    document.getElementById("imgfemale").src = "https://www.flaticon.com/svg/static/icons/svg/390/390914.svg";
   }
   else {
     document.getElementById("imgfemale").src = res.sprites.front_female;
-  } 
+  }
 
   if(res.sprites.front_female == null){
-    document.getElementById("imgfemaleshiny").src = "https://www.flaticon.com/svg/static/icons/svg/390/390914.svg"
+    document.getElementById("imgfemaleshiny").src = "https://www.flaticon.com/svg/static/icons/svg/390/390914.svg";
   }
   else {
     document.getElementById("imgfemaleshiny").src = res.sprites.front_shiny_female;
   } 
-
 })
 .catch(function(error
 ){
   console.log(error);
 })
 
-// Weight & Height of Pokemon
+// Set details of Pokemon
 let url3 = "https://pokeapi.co/api/v2/pokemon/" + search.value
 fetch(url3)
 .then(response => response.json())
@@ -69,7 +58,13 @@ fetch(url3)
   }
   else {
     document.getElementById("type2").innerHTML = res.types[1].type.name;
-  } 
+  }
+
+  // Set name
+  var pokename = res.name;
+  // Capitalize first letter
+  pokename = pokename.charAt(0).toUpperCase() + pokename.slice(1);
+  document.getElementById("name").innerHTML = pokename;
 })
 .catch(function(error
 ){
@@ -85,7 +80,7 @@ fetch(url4)
   // For loop to run through and retrieve the first english description (sometimes the first description may not neccesarily be in english)
   for (i=0; i < res.flavor_text_entries.length; i++){
     if (res.flavor_text_entries[i].language.name == "en"){
-      var string = res.flavor_text_entries[i].flavor_text
+      var string = res.flavor_text_entries[i].flavor_text;
       string = string.replace("\u000c", " ") // Removing the unicode icon that is included in the api string value
       document.getElementById("desc").innerHTML = string;
       break;
@@ -96,47 +91,80 @@ fetch(url4)
 ){
  console.log(error);
 })
-
-
-});
-
-malebtn.addEventListener("click", function() {
-  document.getElementById("imgfemale").style.display = "none"
-  document.getElementById("imgfemaleshiny").style.display = "none"
-  document.getElementById("img").style.display = "inline"
-  document.getElementById("imgshiny").style.display = "inline"
-});
-
-femalebtn.addEventListener("click", function() {
-  document.getElementById("imgfemale").style.display = "inline"
-  document.getElementById("imgfemaleshiny").style.display = "inline"
-  document.getElementById("img").style.display = "none"
-  document.getElementById("imgshiny").style.display = "none"
 });
 
 // Switch between default and shiny image
 defaultbutton.addEventListener("click", function() {
-  console.log("default btn")
+  console.log("default btn");
+
+  // if female shiny displayed, change to female default
   if (document.getElementById("imgfemaleshiny").style.display == "inline"){
-    document.getElementById("imgfemale").style.display = "inline"
-    document.getElementById("imgfemaleshiny").style.display = "none"
+    document.getElementById("imgfemale").style.display = "inline";
+    document.getElementById("imgfemaleshiny").style.display = "none";
   }
-  if (document.getElementById("imgshiny").style.display = "inline"){
-    document.getElementById("img").style.display = "inline"
-    document.getElementById("imgshiny").style.display = "none"
+
+  // if male shiny displayed, change to male default
+  if (document.getElementById("imgshiny").style.display == "inline"){
+    document.getElementById("img").style.display = "inline";
+    document.getElementById("imgshiny").style.display = "none";
   }
 });
 
 shinybutton.addEventListener("click", function() {
-  console.log("shiny btn")
+  console.log("shiny btn");
+
+  // if female displayed, change to female shiny
   if (document.getElementById("imgfemale").style.display == "inline"){
-    document.getElementById("imgfemaleshiny").style.display = "inline"
-    document.getElementById("imgfemale").style.display = "none"
+    document.getElementById("imgfemaleshiny").style.display = "inline";
+    document.getElementById("imgfemale").style.display = "none";
   }
-  if (document.getElementById("img").style.display = "inline"){
-    console.log("shiny change")
-    document.getElementById("imgshiny").style.display = "inline"
-    document.getElementById("img").style.display = "none"
+
+  // if male displayed, change to male shiny
+  if (document.getElementById("img").style.display == "inline"){
+    console.log("shiny change");
+    document.getElementById("imgshiny").style.display = "inline";
+    document.getElementById("img").style.display = "none";
     
   }
 });
+
+malebtn.addEventListener("click", function() {
+  console.log("default btn");
+
+  // if female displayed, change to male
+  if (document.getElementById("imgfemale").style.display == "inline"){
+    console.log("1");
+    document.getElementById("imgfemale").style.display = "none";
+    document.getElementById("img").style.display = "inline";
+  }
+
+  // if female shiny displayed, change to male shiny
+  if (document.getElementById("imgfemaleshiny").style.display == "inline"){
+    console.log("2");
+    document.getElementById("imgfemaleshiny").style.display = "none";
+    document.getElementById("imgshiny").style.display = "inline";
+  }
+});
+
+femalebtn.addEventListener("click", function() {
+  console.log("default btn")
+
+  // if male displayed, change to female
+  if (document.getElementById("img").style.display == "inline"){
+    document.getElementById("img").style.display = "none";
+    document.getElementById("imgfemale").style.display = "inline";
+  }
+
+  // if male shiny displayed, change to female shiny
+  if (document.getElementById("imgshiny").style.display == "inline"){
+    document.getElementById("imgshiny").style.display = "none";
+    document.getElementById("imgfemaleshiny").style.display = "inline";
+  }
+});
+
+function clearImage(){
+  document.getElementById("imgfemaleshiny").style.display = "none";
+  document.getElementById("imgfemale").style.display = "none";
+  document.getElementById("imgshiny").style.display = "none";
+  document.getElementById("img").style.display = "none";
+};
