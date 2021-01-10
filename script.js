f81c5c75c659fcdb315e2493830eeab0e4e9417a
conanxd45 <conanxd45@gmail.com>
2021-01-10 11:18:47 +0000

    Added Search Function( for types for description (checker to ensure description returned is only in english) for shiny image )

--- script.js
+++ script.js
@@ -13,26 +13,61 @@ function data (e) {
 
 submit.addEventListener("click", function() {
 let search = document.getElementById("srch");
-let url2 = "https://pokeapi.co/api/v2/pokemon-form/" + search.value
-console.log(url2)
 
+// Image of Pokemon
+let url2 = "https://pokeapi.co/api/v2/pokemon-form/" + search.value;
 fetch(url2)
 .then(response => response.json())
 .then(function(res){
-  var test = res.sprites.front_default
-  document.getElementById("img").src = test;
+  document.getElementById("img").src = res.sprites.front_default;
+  document.getElementById("imgshiny").src = res.sprites.front_shiny;
 })
 .catch(function(error
 ){
   console.log(error);
 })
 
+// Weight & Height of Pokemon
 let url3 = "https://pokeapi.co/api/v2/pokemon/" + search.value
 fetch(url3)
 .then(response => response.json())
 .then(function(res){
+
+  // Set weight & height
   document.getElementById("weight").innerHTML = res.weight / 10 + " kg";
   document.getElementById("height").innerHTML = res.height / 10 + " m";
+
+  // Set first type
+  document.getElementById("type1").innerHTML = res.types[0].type.name;
+  
+  // Check if second type null, else set second type (some pokemon only has one type)
+  if(res.types[1] == null){
+    document.getElementById("type2").innerHTML = "";
+  }
+  else {
+    document.getElementById("type2").innerHTML = res.types[1].type.name;
+  } 
+})
+.catch(function(error
+){
+ console.log(error);
+})
+
+// Description of Pokemon
+let url4 = "https://pokeapi.co/api/v2/pokemon-species/" + search.value
+fetch(url4)
+.then(response => response.json())
+.then(function(res){
+
+  // For loop to run through and retrieve the first english description (sometimes the first description may not neccesarily be in english)
+  for (i=0; i < res.flavor_text_entries.length; i++){
+    if (res.flavor_text_entries[i].language.name == "en"){
+      var string = res.flavor_text_entries[i].flavor_text
+      string = string.replace("\u000c", " ") // Removing the unicode icon that is included in the api string value
+      document.getElementById("desc").innerHTML = string;
+      break;
+    }
+  }
 })
 .catch(function(error
 ){
